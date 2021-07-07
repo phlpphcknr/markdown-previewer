8cda3dff01f247327f3c03edaf191e23c905cc83
phlpphcknr <philipp.hackner@protonmail.com>
2021-07-07 16:59:24 +0200

    First draft for terraform

---
+++ main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    # Cloud Run support was added on 3.3.0
+    google = ">= 3.3"
+  }
+}
+
+locals {
+  project_id = "markdown-terraform"
+  region = "europe-west1"
+}
+
+provider "google" {
+  credentials = test
+  project = local.project_id
+  region = local.region
+  zone = "europe-west1b"
+}
+
+# Enables the Cloud Run API
+resource "google_project_service" "run_api" {
+  service = "run.googleapis.com"
+  disable_on_destroy = true
+}
+
+
+resource "google_artifact_registry_repository" "markdown-app" {
+  project = local.project_id
+  location = local.region
+  repository_id = "markdown-previewer"
+  format = "DOCKER"
+}
+
+resource "google_cloud_run_service" "cloud_run_service" {
+  name = "markdown-tf"
+  project = local.project_id
+  location = local.region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/markdown-terraform/markdown-app"
+        env {
+          name = "google.auth.client-id"
+          value= var.googleauthclientid
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  # Waits for the Cloud Run API to be enabled
+  depends_on = [google_project_service.run_api]
+
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+# Allow unauthenticated users to invoke the service
+//resource "google_cloud_run_service_iam_member" "run_all_users" {
+//  service  = google_cloud_run_service.cloud_run_service.name
+//  location = google_cloud_run_service.cloud_run_service.location
+//  role     = "roles/run.invoker"
+//  member   = "allUsers"
+//}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.cloud_run_service.location
+  project     = google_cloud_run_service.cloud_run_service.project
+  service     = google_cloud_run_service.cloud_run_service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
